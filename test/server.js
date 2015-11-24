29f644ce455478adcd2a84d1a3fcafef32390375
Ivan <ivan@sq10.net>
2015-11-24 10:17:46 -0600

    readme, example test

---
+++ test/server.js
@@ -0,0 +1,11 @@
+var socketeer = require('../index.js')
+var expect = require('chai').expect
+
+describe('SocketeerServer', function(){
+    describe('[construct]', function(){
+        it('should construct without any arguments', function(){
+            var server = new socketeer.Server()
+            expect(server).to.be.an.instanceof(socketeer.Server)
+        })
+    })
+})
