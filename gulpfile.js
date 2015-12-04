200d8a86fcbe29a25d555b0905605d86c142ee46
Ivan <ivan@sq10.net>
2015-12-04 14:00:08 -0600

    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp')
+var babel = require('gulp-babel')
+var standard = require('gulp-standard')
+var shell = require('gulp-shell')
+
+gulp.task('default', ['build', 'docs'])
+
+gulp.task('build', function () {
+  return gulp.src('src/index.js')
+    .pipe(standard())
+    .pipe(standard.reporter('default', {breakOnError: true}))
+    .pipe(babel())
+    .pipe(gulp.dest('lib/'))
+})
+
+gulp.task('docs', shell.task([
+  'jsdoc -d doc -r ./src'
+]))
