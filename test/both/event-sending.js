52e0d0e5ccf4e8b24451b1b94f15595521c08d5c
ivan <ivan@sq10.net>
2016-01-26 20:15:41 -0600

    test: fix event-sending to actually work

--- test/both/event-sending.js
+++ test/both/event-sending.js
@@ -1,27 +1,33 @@
 import test from 'ava'
 import {Server, Client} from '../../'
 
-test(`server and client should correctly send and receive events`, async (t) => {
+test(`server and client should correctly send and receive events`, (t) => {
   t.plan(4)
+  return new Promise((resolve, reject) => {
+    const server = new Server()
+    const client = new Client('ws://127.0.0.1:42424/')
+    server.on('error', reject)
+    client.on('error', reject)
+    server.listen(42424).then(() => {
+      server.on('connection', (sclient) => {
+        sclient.on('error', reject)
+        sclient.emit('hello', 'yes')
+        sclient.event('bye', (data) => {
+          t.is(data, 'no')
+          resolve()
+        })
+      })
 
-  const server = new Server()
-  server.on('connection', (client) => {
-    client.emit('hello', 'yes')
-    client.event('bye', (data) => {
-      t.is(data, 'no')
+      let isOpen = false
+      client.once('open', () => {
+        t.pass()
+        isOpen = true
+      })
+      client.event('hello', (data) => {
+        t.is(isOpen, true)
+        t.is(data, 'yes')
+        client.emit('bye', 'no')
+      })
     })
-  })
-  await server.listen(42424)
-
-  const client = new Client('ws://127.0.0.1/')
-  let isOpen = false
-  client.once('open', () => {
-    t.pass()
-    isOpen = true
-  })
-  client.event('hello', (data) => {
-    t.is(isOpen, true)
-    t.is(data, 'yes')
-    client.emit('bye', 'no')
   })
 })
