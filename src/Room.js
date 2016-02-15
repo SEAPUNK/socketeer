a5b121a0948239900ce07c051eeab9a646b36f47
Ivan <ivan@sq10.net>
2016-02-15 14:51:58 -0600

    convert Room to es6

--- src/Room.js
+++ src/Room.js
@@ -1,49 +1,46 @@
 'use strict'
 
-var debug = require('debug')
+const debug = require('debug')
 
-function Room (name) {
-  if (this instanceof Room === false) {
-    return new Room(name)
+class Room {
+  constructor (name) {
+    if (!name) throw new Error('no name specified')
+    this.name = name
+    this._d = debug(`socketeer:Room[${name}]`)
+    this._d('constructing new instance')
+    this._clients = new Set()
   }
-  if (!name) throw new Error('no name specified')
-  var self = this
-  self.name = name
-  self._d = debug('socketeer:Room[' + name + ']')
-  self._d('constructing new instance')
-  self._clients = []
-}
 
-Room.prototype.add = function add (client) {
-  this._d('adding client to room: ' + client.id)
-  if (!this.exists(client)) return false
-  this._clients.push(client)
-  return true
-}
+  add (client) {
+    this._d(`adding client to room: ${client.id}`)
+    if (!this.exists(client)) return false
+    this._clients.add(client)
+    return true
+  }
 
-Room.prototype.remove = function remove (client) {
-  this._d('removing client from room: ' + client.id)
-  if (!this.exists(client)) return false
-  var idx = this._clients.indexOf(client)
-  this._clients.splice(idx, 1)
-  return true
-}
+  remove (client) {
+    this._d(`removing client from room: ${client.id}`)
+    if (!this.exists(client)) return false
+    this._clients.delete(client)
+    return true
+  }
 
-Room.prototype.exists = function exists (client) {
-  this._d('checking if client exists in room: ' + client.id)
-  return this._clients.indexOf(client) !== -1
-}
+  exists (client) {
+    this._d(`checking if client exists in room: ${client.id}`)
+    return this._clients.get(client)
+  }
 
-Room.prototype.emit = function emit (name, data) {
-  this._d('emitting ' + name + ' to all clients in room')
-  this._clients.forEach(function _clientsForEach (client) {
-    client.emit(name, data)
-  })
-}
+  emit (name, data) {
+    this._d(`emitting ${name} to all clients in room`)
+    for (let client of this._clients) {
+      client.emit(name, data)
+    }
+  }
 
-Room.prototype.clear = function clear () {
-  this._d('clearing room from clients')
-  this._clients = []
+  clear () {
+    this._d('clearing room from clients')
+    this._clients = new Set()
+  }
 }
 
 module.exports = Room
