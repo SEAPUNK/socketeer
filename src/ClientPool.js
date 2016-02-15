3bdc0bb0e8fd1023118b22488e054f7c7580477b
Ivan <ivan@sq10.net>
2016-02-15 15:00:18 -0600

    rewrite ClientPool to ES6

--- src/ClientPool.js
+++ src/ClientPool.js
@@ -1,55 +1,56 @@
 'use strict'
 
-var uuid = require('uuid').v4
-var debug = require('debug')
+const uuid = require('uuid').v4
+const debug = require('debug')
 
-function ClientPool () {
-  if (this instanceof ClientPool === false) {
-    return new ClientPool()
+class ClientPool {
+  constructor () {
+    this._d = debug('socketeer:ClientPool')
+    this.pool = new Map()
+    this._reservedIds = new Set()
   }
-  this._d = debug('socketeer:ClientPool')
-  this.pool = {}
-  this._reserved = {}
-}
 
-ClientPool.prototype.add = function add (client, id) {
-  this._d('adding client to pool: ' + id)
-  if (this.pool[id]) throw new Error('id ' + id + ' is already in the pool (should never happen)')
-  delete this._reserved[id]
-  this.pool[id] = client
-  return id
-}
-
-ClientPool.prototype.get = function get (id) {
-  this._d('getting client with id: ' + id)
-  return this.pool[id]
-}
-
-ClientPool.prototype._generateId = function _generateId () {
-  this._d('generating and reserving a new id')
-  var id
-  while (true) {
-    id = uuid()
-    if (!this.pool[id] && !this._reserved[id]) break
+  add (client, id) {
+    this._d(`adding client to pool: ${id}`)
+    if (this.pool.get(id)) throw new Error(`id ${id} is already in the pool`)
+    this._reservedIds.delete(id)
+    this.pool.add(id, client)
+    return id
   }
-  this._reserved[id] = true
-  this._d('generated a client pool id: ' + id)
-  return id
-}
 
-ClientPool.prototype.forEach = function forEach (fn) {
-  this._d('running a foreach function on pool')
-  for (var id in this.pool) fn(this.pool[id])
-}
+  get (id) {
+    this._d(`getting client with id: ${id}`)
+    return this.pool.get(id)
+  }
 
-ClientPool.prototype.remove = function remove (id) {
-  this._d('removing client from pool: ' + id)
-  delete this.pool[id]
-}
+  generateId () {
+    this._d('generating and reserving a new client id')
+    let id
+    while (true) {
+      id = uuid()
+      if (!this.pool.get(id) && !this._reservedIds.has(id)) break
+    }
+    this._reservedIds.add(id)
+    this._d(`generated a client id: ${id}`)
+    return id
+  }
 
-ClientPool.prototype.clear = function clear () {
-  this._d('clearing pool')
-  this.pool = {}
+  forEach (fn) {
+    this._d('running a foreach function on pool')
+    for (let client of this.pool.values()) {
+      fn(client)
+    }
+  }
+
+  remove (id) {
+    this._d(`removing client from pool: ${id}`)
+    this.pool.delete(id)
+  }
+
+  clear () {
+    this._d('clearing pool')
+    this.pool = new Map()
+  }
 }
 
 module.exports = ClientPool
