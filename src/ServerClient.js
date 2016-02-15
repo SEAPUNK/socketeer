a52fd4a3993d0e55388760f53fa970072cb84827
Ivan <ivan@sq10.net>
2016-02-15 16:55:30 -0600

    more work on ServerClient

--- src/ServerClient.js
+++ src/ServerClient.js
@@ -1,12 +1,39 @@
 'use strict'
 
+const debug = require('debug')
 const ClientAbstract = require('./ClientAbstract')
 
 class ServerClient extends ClientAbstract {
+  constructor (ws, server) {
+    super()
+    this.ws = ws
+    this._d = debug('socketeer:ServerClient')
+    this.ip = ws._socket.remoteAddress
+    this._d(`new ServerClient from IP address: ${this.ip}`)
+    this.server = server
+    // TODO: Begin the handshake.
+  }
+
+  _replaceSocket (ws) {
+    this._d(`hot-swapping websockets for ServerClient id ${ws}`)
+    // TODO: Detach current ws's events.
+    this.ws = ws
+    this.ip = ws._socket.remoteAddress
+    this._d(`hot-swapped websocket's IP address: ${this.ip}`)
+    // TODO: Attach this websocket's events.
+    // TODO: Resume the message feed.
+  }
+
   _register () {
-    this.pool.add(client, id)
-    this.room._joinAll(client)
-    this.emit('connection', client)
+    this.server.pool.add(this, this.id)
+    this.server.room._joinAll(this)
+    this.server.emit('connection', this)
+  }
+
+  _awaitHandshake () {
+    return new Promise((resolve, reject) => {
+
+    })
   }
 }
 
