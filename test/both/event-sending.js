ce520f45f8d087d5c001b0ec543b23ae2a84eb47
Ivan <ivan@sq10.net>
2016-02-16 12:00:07 -0600

    fix event emitting test
    client connects before server has time to start

--- test/both/event-sending.js
+++ test/both/event-sending.js
@@ -5,19 +5,20 @@ test('server and client should correctly send and receive events', (t) => {
   t.plan(4)
   return new Promise((resolve, reject) => {
     const server = new Server()
-    const client = new Client('ws://127.0.0.1:42424/')
-    server.on('error', reject)
-    client.on('error', reject)
+    let client
+    server.on('error', (err) => reject(new Error('Server errored out: ' + err)))
     server.listen(42424).then(() => {
       server.on('connection', (sclient) => {
-        sclient.on('error', reject)
+        sclient.on('error', (err) => reject(new Error('Server client errored out: ' + err)))
         sclient.emit('hello', 'yes')
         sclient.event('bye', (data) => {
           t.is(data, 'no')
           resolve()
         })
       })
-
+      client = new Client('ws://127.0.0.1:42424/')
+      client.on('error', (err) => reject(new Error('Client errored out: ' + err)))
+      client.on('close', () => reject(new Error('Connection was closed.')))
       let isOpen = false
       client.once('open', () => {
         t.pass()
