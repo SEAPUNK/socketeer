SEAPUNK/socketeer

commit 0bec7c60fe34be5ac98dc48fedc97269d6eb7620

ServerClient + ClientAbstract: replace _doNotEmitClose with _closeIsPause

src/ClientAbstract.js
@@ -25,7 +25,6 @@ class ClientAbstract extends EventEmitter {
     // The message queue is paused by default.
     this._messageQueue.pause()
 
-    this._doNotEmitClose = false
     this._closeMustHaveError = false
     this._socketeerClosing = false
 
@@ -168,11 +167,10 @@ class ClientAbstract extends EventEmitter {
       // This means we are a Client, and we attempted a session resume.
       // We *should* have this function.
       this._resolveSessionResume(false)
-    } else if (!this._doNotEmitClose) {
-      this._emit('close', code, message, error)
-    } else {
-      // Do nothing.
     }
+
+    const eventName = (this._closeIsPause) ? 'pause' : 'close'
+    this._emit(eventName, code, message, error)
   }
 
   _processQueue (msg, done) {

src/ServerClient.js
@@ -21,8 +21,7 @@ class ServerClient extends ClientAbstract {
     // Our custom _handleClose handler is going to emit a 'pause' event,
     // to indicate the session no longer connected, but the session reconnect
     // timeout will emit the real 'close' event.
-    // TODO: Change this to closeIsPause
-    this._doNotEmitClose = this.server.supportsResuming
+    this._closeIsPause = this.server.supportsResuming
     this.ws = ws
     this.ip = ws._socket.remoteAddress
     this._d(`new ServerClient from IP address: ${this.ip}`)
@@ -201,12 +200,7 @@ class ServerClient extends ClientAbstract {
       this.pool.remove(this.id)
     }
 
-    // TODO: Change this to closeIsPause
     super._handleClose(closeEvent)
-
-    if (this.server.supportsResuming) {
-      return this._emit('pause', closeEvent.code, closeEvent.reason, closeEvent.error || null)
-    }
   }
 
   _replaceSocket (ws, newToken) {

commit 1a38b377e5a4008b48cf4fcdde6bc1abd359168b

ClientAbstract: fix regression introduced in 0bec7c6
The _resolveSessionResume call in _handleClose() should not fall through to also emit an event.

src/ClientAbstract.js
@@ -167,10 +167,10 @@ class ClientAbstract extends EventEmitter {
       // This means we are a Client, and we attempted a session resume.
       // We *should* have this function.
       this._resolveSessionResume(false)
+    } else {
+      const eventName = (this._closeIsPause) ? 'pause' : 'close'
+      this._emit(eventName, code, message, error)
     }
-
-    const eventName = (this._closeIsPause) ? 'pause' : 'close'
-    this._emit(eventName, code, message, error)
   }
 
   _processQueue (msg, done) {