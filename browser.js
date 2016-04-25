9d2e27108c6c4181853f9578ec902d9bd317e2d0
Ivan <ivan@sq10.net>
2016-04-25 14:04:31 -0500

    browser.js, for providing locations to browser builds

--- browser.js
+++ browser.js
@@ -1,5 +1,35 @@
 'use strict'
 
-const fs = require('fs')
+const path = require('path')
 
+const libraryTargets = [
+  'amd',
+  'var',
+  'commonjs2'
+]
 
+const suffixes = [
+  'debug',
+  'min',
+  'plain'
+]
+
+const paths = {}
+
+for (let target of libraryTargets) {
+  paths[target] = {}
+  for (let suffix of suffixes) {
+    let filename = [
+      'socketeer-browser',
+      target
+    ]
+    if (suffix !== 'plain') {
+      filename.push(suffix)
+    }
+    filename.push('js')
+
+    paths[target][suffix] = path.join(__dirname, `browser-builds/${filename.join('.')}`)
+  }
+}
+
+module.exports = paths
